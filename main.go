68e013bf9715a8e80b9c2c27e60342abddab5d0a
Евгений Газетдинов <e.gazetdinov@litebox.ru>
2021-06-23 15:07:54 +0300

    add basic implement // TODO fix equlize bugs

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"strconv"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -28,40 +29,51 @@ func get_variable_from_uri(variable_name string, w http.ResponseWriter, r *http.
 	return variable
 }
 
-func return_variables_for_equalize(one_name string, second_name string, w http.ResponseWriter, r *http.Request) ([]string, []string) {
+func return_variables_for_equalize(one_name string, second_name string, w http.ResponseWriter, r *http.Request) (uint64, uint64) {
+
 	one := get_variable_from_uri(one_name, w, r)
 	two := get_variable_from_uri(second_name, w, r)
-	return one, two
+	first_value, err := strconv.ParseUint(one[0], 16, 64)
+	second_value, err := strconv.ParseUint(two[0], 16, 64)
+	fmt.Println((err))
+	return first_value, second_value
+
 }
 
 func div(w http.ResponseWriter, r *http.Request) {
 	first, second := return_variables_for_equalize("a", "b", w, r)
-	fmt.Println(second[0])
 	//TODO add hanler for equalize two variales
-	succes_place(fmt.Sprint(first[0]), w, r)
+	result := first / second
+	succes_place(fmt.Sprint(result), w, r)
 }
+
 func add(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(1)
+	first, second := return_variables_for_equalize("a", "b", w, r)
+	//TODO add hanler for equalize two variales
+	result := first + second
+	succes_place(fmt.Sprint(result), w, r)
 }
+
 func sub(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(1)
+	first, second := return_variables_for_equalize("a", "b", w, r)
+	//TODO add hanler for equalize two variales
+	result := first - second
+	succes_place(fmt.Sprint(result), w, r)
 }
+
 func mul(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(1)
+	first, second := return_variables_for_equalize("a", "b", w, r)
+	//TODO add hanler for equalize two variales
+	result := first * second
+	succes_place(fmt.Sprint(result), w, r)
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/add/", add)
 	router.HandleFunc("/api/div/", div)
-	router.HandleFunc("/sub/", sub)
-	router.HandleFunc("/mul/", mul)
+	router.HandleFunc("/api/sub/", sub)
+	router.HandleFunc("/api/mul/", mul)
 	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
 	log.Println("listening on 8080")
 	http.ListenAndServe(":8080", loggedRouter)
